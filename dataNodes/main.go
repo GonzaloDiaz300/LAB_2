a44f69d57ea35c16baaee88952165e532313d1d9
prettydoggo <52218179+nautt@users.noreply.github.com>
2023-10-12 16:16:13 -0300

    gomod gosum

--- dataNodes/main.go
+++ dataNodes/main.go
@@ -1,1 +1,46 @@
 package datanodes
+
+import (
+	"fmt"
+	"net"
+
+	pb "github.com/GonzaloDiaz300/LAB_2/proto"
+	"google.golang.org/grpc"
+)
+
+/*
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	pb "github.com/GonzaloDiaz300/LAB_2/proto"
+)
+*/
+
+type serverNode struct {
+	pb.UnimplementedNotificacionServer
+}
+
+func main() {
+	//Puerto oms
+	listner, err := net.Listen("tcp", ":50056")
+
+	if err != nil {
+		panic("cannot create tcp connection" + err.Error())
+	}
+
+	serv := grpc.NewServer()
+	fmt.Printf("ServerNode Activo\n")
+	pb.RegisterNotificacionServer(serv, &serverNode{})
+	if err = serv.Serve(listner); err != nil {
+		panic("cannot initialize the server" + err.Error())
+	}
+}
