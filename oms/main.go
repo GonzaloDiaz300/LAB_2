de87d41d9cd20ddb9f1f725bbb783959e9738c90
sebastianvargas-m <72326137+sebastianvargas-m@users.noreply.github.com>
2023-10-18 00:31:51 -0300

    actualizando oms (no actualizo)

--- oms/main.go
+++ oms/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"sync"
 
 	pb "github.com/GonzaloDiaz300/LAB_2/proto"
 	"google.golang.org/grpc"
@@ -35,13 +34,57 @@ var ID_datanode2 = 1 //IDs que se le asignaran al datanode2, que seran siempre n
 
 var flag bool
 
-func (a *oms) Notificar(ctx context.Context, in *pb.ContiReq) (*pb.ContiResp, error) {
-	fmt.Printf("Estado Recibido: %s %s %s \n", in.Nombre, in.Apellido, in.Estado)
-	return &pb.ContiResp{Respuesta: 1}, nil
+func (a *oms) Notificar(ctx context.Context, in *pb.ContiReq) (*pb.Confirmacion, error) {
+	fmt.Printf("Estado Recibido: %s %s %s \n", in.Estado)
+	//Se envía la id a los DataNodes
+	if "A" <= string(in.Apellido[0]) && string(in.Apellido[0]) <= "M" {
+		go enviarMensaje(DataNodes[0], ID_datanode1, in.Nombre, in.Apellido)
+		linea := fmt.Sprintf("%d %s %s\n", ID_datanode1, in.Nombre, in.Apellido)
+		go escribir_archivo(linea)
+		ID_datanode1 += 2
+	} else if "N" <= string(in.Apellido[0]) && string(in.Apellido[0]) <= "Z" {
+		go enviarMensaje(DataNodes[1], ID_datanode2, in.Nombre, in.Apellido)
+		linea := fmt.Sprintf("%d %s %s\n", ID_datanode2, in.Estado)
+		go escribir_archivo(linea)
+		ID_datanode2 += 2
+	}
+	return &pb.Confirmacion{Respuesta: 1}, nil
 }
 
-func enviarMensaje(datanode string, id int, nombre string, apellido string, wg *sync.WaitGroup) {
-	defer wg.Done()
+func (a *oms) Nombres(ctx context.Context, in *pb.ONUReq) (*pb.ONUResp, error) {
+	return &pb.ONUResp{}, nil
+}
+
+func escribir_archivo(linea string) {
+	// Nombre del archivo que deseas trabajar
+	filename := "DATA.txt"
+
+	// Intentar abrir el archivo en modo lectura
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Si el archivo no existía previamente, escribir datos en él
+	if _, err = file.Stat(); os.IsNotExist(err) {
+		_, err = file.Write([]byte(linea))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Archivo creado exitosamente.")
+	} else {
+		_, err = file.Write([]byte(linea))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+func enviarMensaje(datanode string, id int, nombre string, apellido string) {
 	// Aquí puedes implementar la lógica para enviar el mensaje al servidor especificado
 	conn, err := grpc.Dial(datanode, grpc.WithInsecure())
 	if err != nil {
@@ -50,7 +93,7 @@ func enviarMensaje(datanode string, id int, nombre string, apellido string, wg *
 	}
 	defer conn.Close()
 	serviceClient := pb.NewIntercambiosClient(conn)
-	res, err := serviceClient.Buscar(context.Background(), &pb.OMSReq{Id: int32(id), Nombre: nombre, Apellido: apellido})
+	res, err := serviceClient.Guardar(context.Background(), &pb.OMSReq{Id: int32(id), Nombre: nombre, Apellido: apellido})
 	if err != nil {
 		panic("No se llego el mensaje " + err.Error())
 	}
@@ -60,14 +103,6 @@ func enviarMensaje(datanode string, id int, nombre string, apellido string, wg *
 }
 
 func main() {
-
-	//se crea el archivo DATA.txt
-	archivo, err := os.Create("DATA.txt")
-	if err != nil {
-		fmt.Println("Error al crear el archivo:", err)
-		return
-	}
-	defer archivo.Close()
 
 	listner, err := net.Listen("tcp", ":50051")
 	if err != nil {
@@ -81,24 +116,4 @@ func main() {
 		panic("cannot initialize the server" + err.Error())
 	}
 
-	//Tiene que recibir los nombres de los continentes primero
-	var apellido string
-	var nombre string
-
-	var wg sync.WaitGroup
-	//Se envía la id a los DataNodes
-	if "A" <= string(apellido[0]) && string(apellido[0]) <= "M" {
-		wg.Add(1)
-		go enviarMensaje(DataNodes[0], ID_datanode1, nombre, apellido, &wg)
-		linea := fmt.Sprintf("%d %s %s\n", ID_datanode1, nombre, apellido) //Se crea la línea que se escribirá en el txt
-		archivo.WriteString(linea)                                         //Se escribe en el txt
-		ID_datanode1 += 2
-	} else if "N" <= string(apellido[0]) && string(apellido[0]) <= "Z" {
-		wg.Add(1)
-		go enviarMensaje(DataNodes[1], ID_datanode2, nombre, apellido, &wg)
-		linea := fmt.Sprintf("%d %s %s\n", ID_datanode2, nombre, apellido) //Se crea la línea que se escribirá en el txt
-		archivo.WriteString(linea)                                         //Se escribe en el txt
-		ID_datanode2 += 2
-	}
-	wg.Wait()
 }
